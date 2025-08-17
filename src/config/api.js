b7417a210b02d77b3f6808044ceb0445d2dcc866
Nitesh Shetye <niteshshetye204@gmail.com>
2025-08-17 13:03:15 +0530

    updated base url as per nginx config

--- src/config/api.js
+++ src/config/api.js
@@ -1,19 +1,20 @@
-export const BASE_URL = "http://localhost:3000/api/v1";
+export const BASE_URL = "/api/v1";
 
 export const AUTH_URLS = {
-	LOGIN: `${BASE_URL}/auth/login`, LOGOUT: `${BASE_URL}/auth/logout`,
+  LOGIN: `${BASE_URL}/auth/login`,
+  LOGOUT: `${BASE_URL}/auth/logout`,
 };
 
 export const USER_URLS = {
-	DETAILS: `${BASE_URL}/profile/view`,
-	UPDATE: `${BASE_URL}/profile/edit`,
-	DELETE: `${BASE_URL}/profile/delete`,
-	HOME_FEED: `${BASE_URL}/user/feed`,
-	CONNECTIONS: `${BASE_URL}/user/feed/request/connection`,
-	REQUESTS: `${BASE_URL}/user/feed/request/received`,
+  DETAILS: `${BASE_URL}/profile/view`,
+  UPDATE: `${BASE_URL}/profile/edit`,
+  DELETE: `${BASE_URL}/profile/delete`,
+  HOME_FEED: `${BASE_URL}/user/feed`,
+  CONNECTIONS: `${BASE_URL}/user/feed/request/connection`,
+  REQUESTS: `${BASE_URL}/user/feed/request/received`,
 };
 
 export const REQUESTS_URLS = {
-	REVIEW_REQUEST: `${BASE_URL}/request/review`,
-	CREATE_CONNECTIONS: `${BASE_URL}/request/send`,
+  REVIEW_REQUEST: `${BASE_URL}/request/review`,
+  CREATE_CONNECTIONS: `${BASE_URL}/request/send`,
 };
